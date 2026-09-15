afb148e5c34098e4e7e951e3ab3bf86a0ae1f755
oskarssss <oskars.ruika@gmail.com>
2026-09-15 14:52:40 +0300

    Jauna klase un konstruktors

---
+++ bank_sys.java
@@ -0,0 +1,14 @@
+class BankAccount {
+    private String owner;
+    private double balance;
+    private ArrayList<String> transactions;
+
+    public BankAccount(String owner, double balance) {
+        this.owner = owner;
+        this.balance = balance;
+        this.transactions = new ArrayList<>();
+    }
+
+    transactions.add("Konts izveidots ar atlikumu: " + balance + " $")
+    
+}
