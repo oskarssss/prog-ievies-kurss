2dc1134aab5dfb6f59bde81b8b4b6fbd96ec7536
oskarssss <oskars.ruika@gmail.com>
2026-09-15 15:03:34 +0300

    Main klase, scanner ieviešana, input

--- bank_sys.java
+++ bank_sys.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class BankAccount {
     private String owner;
     private double balance;
@@ -7,8 +9,28 @@ class BankAccount {
         this.owner = owner;
         this.balance = balance;
         this.transactions = new ArrayList<>();
+
+        transactions.add("Konts izveidots ar atlikumu: " + balance + " $");
     }
 
-    transactions.add("Konts izveidots ar atlikumu: " + balance + " $")
+    public class Main {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("----- BANKA -----");
+
+            System.out.println("Ievadi savu vārdu: ");
+            String name = scanner.nextLine();
+
+            System.out.print("Ievadi sākuma atlikumu: ");
+            double startingBalance = scanner.nextDouble();
+
+            BankAccount account = new BankAccount(name, startingBalance);
+        }
+    }
+
     
+
+
+
 }
